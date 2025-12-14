6530fd830052acd01db6e5e12e26b002b0a5b5f0
arpcothirteen <arpcothirteen@outlook.com>
2025-12-14 17:22:48 +0330

    last commit of 23rd

--- src/app/pages/Auth/sign-up-form/sign-up-form.ts
+++ src/app/pages/Auth/sign-up-form/sign-up-form.ts
@@ -6,11 +6,12 @@ import { EmailValidationService } from '../../../services/validation/email-valid
 import { NationalCodeValidationService } from '../../../services/validation/national-code-validation';
 import { NameValidationService } from '../../../services/validation/name-validation';
 import { LastNameValidationService } from '../../../services/validation/lastname-validation-service';
+import { SelectField, Option } from '../../../components/select-field/select-field';
 
 @Component({
   selector: 'app-sign-up-form',
   standalone: true,
-  imports: [TextField, ReactiveFormsModule],
+  imports: [TextField, ReactiveFormsModule, SelectField],
   templateUrl: './sign-up-form.html',
 })
 export class SignUpForm {
@@ -33,6 +34,31 @@ export class SignUpForm {
   public nationalCodeUnmet = signal<string[]>([]);
   public firstnameUnmet = signal<string[]>([]);
   public lastnameUnmet = signal<string[]>([]);
+  public legalPersoncompnameUnmet = signal<string[]>([]);
+  public legalPersonregisterynumberUnmet = signal<string[]>([]);
+  public legalPersonNationalidUnmet = signal<string[]>([]);
+  public legalPersonEconomicCodeUnmet = signal<string[]>([]);
+  public postalCodeUnmet = signal<string[]>([]);
+  public realPersonnameUnmet = signal<string[]>([]);
+  public realPersonNationalcodeUnmet = signal<string[]>([]);
+  public realpersonEconomicCodeUnmet = signal<string[]>([]);
+  public partnershipNameUnmet = signal<string[]>([]);
+  public partnershipEconomicCodeUnmet = signal<string[]>([]);
+
+  public customerTypes: Option[] = [
+    {
+      value: 'real',
+      label: 'حقیقی',
+    },
+    {
+      value: 'legal',
+      label: 'حقوقی',
+    },
+    {
+      value: 'partnership',
+      label: 'مشارکت مدنی',
+    },
+  ];
 
   constructor() {
     this.signUpForm = this.fb.group({
@@ -41,10 +67,35 @@ export class SignUpForm {
       nationalCode: ['', [Validators.required]],
       email: ['', [Validators.required]],
       password: ['', [Validators.required]],
-      rules: [false, [Validators.requiredTrue]],
+      rulesAccepted: [false, [Validators.requiredTrue]],
+      isRepresentative: [false],
+      representativeType: [null],
+      legalPersoncompname: [''],
+      legalPersonregisterynumber: [''],
+      legalPersonNationalid: [''],
+      legalPersonEconomicCode: [''],
+      postalCode: [''],
+      realPersonname: [''],
+      realPersonNationalcode: [''],
+      realpersonEconomicCode: [''],
+      partnershipName: [''],
+      partnershipEconomicCode: [''],
     });
     this.updateValidationSignals(this.signUpForm.value);
-    // Listen to changes to update validation signals
+    this.signUpForm.get('isRepresentative')?.valueChanges.subscribe((isChecked) => {
+      if (!isChecked) {
+        this.signUpForm.get('representativeType')?.setValue(null);
+        this.signUpForm.get('representativeType')?.clearValidators();
+        this.clearDynamicFields();
+      } else {
+        this.signUpForm.get('representativeType')?.setValidators([Validators.required]);
+      }
+      this.signUpForm.get('representativeType')?.updateValueAndValidity();
+    });
+    this.signUpForm.get('representativeType')?.valueChanges.subscribe((selection) => {
+      const representativeValue = selection?.value;
+      // this.handleRepTypeChange(representativeValue);
+    });
     this.signUpForm.valueChanges.subscribe((val) => {
       this.updateValidationSignals(val);
     });
@@ -72,6 +123,34 @@ export class SignUpForm {
     this.passwordColor.set(passResult.extra.color);
     this.passwordPercentage.set(passResult.extra.percentage);
     this.passwordUnmet.set(passResult.helper);
+  }
+
+  clearDynamicFields() {
+    const allDynamicFileds = [
+      'legalPersoncompname',
+      'legalPersonregisterynumber',
+      'legalPersonNationalid',
+      'legalPersonEconomicCode',
+      'postalCode',
+      'realPersonname',
+      'realPersonNationalcode',
+      'realpersonEconomicCode',
+      'partnershipName',
+      'partnershipEconomicCode',
+    ];
+    allDynamicFileds.forEach((field) => {
+      const control = this.signUpForm.get(field);
+      control?.clearValidators();
+      control?.updateValueAndValidity();
+    });
+  }
+
+  setRequired(fields: string[]) {
+    fields.forEach((field) => {
+      const control = this.signUpForm.get(field);
+      control?.setValidators([Validators.required]);
+      control?.updateValueAndValidity();
+    });
   }
 
   onSubmit() {
